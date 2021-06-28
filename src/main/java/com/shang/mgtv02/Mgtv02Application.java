a0f17ef32a8bc22338f82079ba4fe93625b1996b
jia_shang <630948921@qq.com>
2021-06-28 18:41:51 +0800

    20210628

---
+++ src/main/java/com/shang/mgtv02/Mgtv02Application.java
@@ -0,0 +1,13 @@
+package com.shang.mgtv02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mgtv02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mgtv02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/shang/mgtv02/Mgtv02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shang.mgtv02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mgtv02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
